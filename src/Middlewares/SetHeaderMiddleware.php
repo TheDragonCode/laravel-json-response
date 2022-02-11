b88b3e8199d2b0969211c1433c2558fb40bf0ffc
Andrey Helldar <helldar@ai-rus.com>
2022-02-12 00:21:16 +0300

    Bugfix

--- src/Middlewares/SetHeaderMiddleware.php
+++ src/Middlewares/SetHeaderMiddleware.php
@@ -5,12 +5,13 @@ namespace DragonCode\LaravelJsonResponse\Middlewares;
 use Closure;
 use Illuminate\Http\Request;
 use Lmc\HttpConstants\Header;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SetHeaderMiddleware
 {
     /**
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param Closure $next
      *
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
      */
@@ -19,6 +20,11 @@ class SetHeaderMiddleware
         /** @var \Illuminate\Http\Response $response */
         $response = $next($request);
 
-        return $response->header(Header::ACCEPT, 'application/json');
+        return $this->hasJson($response) ? $response : $response->header(Header::ACCEPT, 'application/json');
+    }
+
+    protected function hasJson($response): bool
+    {
+        return $response instanceof JsonResponse;
     }
 }
